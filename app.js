a24ff7df0e4ee9f3179b1d46e99e1e6b014d6d0d
LauraGrandjean <97133455+LauraGrandjean@users.noreply.github.com>
2025-02-21 19:44:09 -0300

    Add files via upload

--- app.js
+++ app.js
@@ -1,1 +1,16 @@
-console.log (ejecutando el sistema)
+const inputAmigo = document.getElementById ('amigo');
+const listaAmigos = [];
+const ulListaAmigos= document.getElementById('listaAmigos');
+const ulResultado =document.getElementById("resultado");
+
+function agregarAmigo(){
+     listaAmigos.push(inputAmigo.value);
+     ulListaAmigos.innerHTML += `<li>${inputAmigo.value}</li>`;
+}
+
+function sortearAmigo(){
+const random =Math.floor(Math.random()* listaAmigos.length);
+const amigoSecreto = listaAmigos [random];
+ulResultado.innerHTML =`<li>El amigo secreto es : ${amigoSecreto} </li>`;
+
+}
